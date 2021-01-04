1b3acb475e24a1bd2698fe0defb5348d70042a72
Alec <alec.92@hotmail.fr>
2021-01-04 19:28:27 +0100

    Finish Cryptofinance project

--- SelfishMining.py
+++ SelfishMining.py
@@ -1,9 +1,12 @@
 import random
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 
 # INPUT
 # n : number of attack cycles belonging to R+
-# q : relative hashrate belonging to [0;1]
+# q : relative hashrate belonging to [0;0.5]
 # gamma : the ratio of honest miners that choose to mine on the pool’s block belonging to [0;1]
 # bitcoinValue : coinbase belonging to R+
 # bitcoinReward : no. of bitcoin for each block mined belonging to R+
@@ -131,28 +134,120 @@ def timeConverter(n):
         sec=n
     return sec, min, hour, day
 
+#### TEST WITH INPUT DATA ####
+def testSelfishMiningInputData():
+    n = int(input("numberCycle : "))
+    q = float(input("relativeHashrate : "))
+    gamma = float(input("gamma : "))
+    bitcoinValue = int(input("bitcoinValue : "))
+    bitcoinReward = int(input("bitcoinReward : "))
+    inputVariables = {'numberCycles':n, 'relativeHashrate':q, 'gamma':gamma,'bitcoinValue':bitcoinValue,'bitcoinReward':bitcoinReward}
+    return inputVariables
 
-n = int(input("numberCycle : "))
-q = float(input("relativeHashrate : "))
-gamma = float(input("gamma : "))
-bitcoinValue = int(input("bitcoinValue : "))
-bitcoinReward = int(input("bitcoinReward : "))
-inputVariables = {'numberCycles':n, 'relativeHashrate':q, 'gamma':gamma,'bitcoinValue':bitcoinValue,'bitcoinReward':bitcoinReward}
 
-#inputVariables = {'numberCycles':10, 'relativeHashrate':0.25, 'gamma':0.5,'bitcoinValue':10000,'bitcoinReward':6}
+#### AUTOMATIC TEST ####
+def testSelfishMiningAutomaticData():
+    inputVariables = {'numberCycles':1000, 'relativeHashrate':0.4, 'gamma':0,'bitcoinValue':10000,'bitcoinReward':6}
+    return inputVariables
 
-new = SelfishMining(**inputVariables)
-new.Simulation1()
-print('\nreward :', new.profitSelfishMining, end=' dollars\n')
-time = timeConverter(new.time)
-if(time[3] == 0): 
-    print('time :', time[2],'hours', time[1],'minutes', time[0],'seconds')
-elif(time[2] == 0): 
-    print('time :', time[1],'minutes', time[0],'seconds')
-elif(time[1] == 0): 
-    print('time :', time[0],'seconds')
-else:
-    print('time :', time[3],'days', time[2],'hours', time[1],'minutes', time[0],'seconds')
-print('orphanBlocks :', new.orphanBlocks, end=' units\n')
-print('officialChain :', new.officialChain, end=' units\n\n')
+
+#### Display data ### 
+def displayData(data):
+    new = SelfishMining(**data)
+    new.Simulation1()
+    time = timeConverter(new.time)
+    revenueRate = new.profitSelfishMining/(new.officialChain*new.bitcoinReward*new.bitcoinValue)
+    if(time[3] == 0): 
+        print('\ntime :', time[2],'hours', time[1],'minutes', time[0],'seconds')
+    elif(time[2] == 0): 
+        print('\ntime :', time[1],'minutes', time[0],'seconds')
+    elif(time[1] == 0): 
+        print('\ntime :', time[0],'seconds')
+    else:
+        print('\ntime :', time[3],'days', time[2],'hours', time[1],'minutes', time[0],'seconds')
+    print('orphanBlocks :', new.orphanBlocks, end=' units\n')
+    print('officialChain :', new.officialChain, end=' units\n')
+    print('reward :', new.profitSelfishMining, end=' dollars\n')
+    print('revenue rate : ', revenueRate, end='\n\n')
+
+#### Launch selfish mining test with all data #### 
+def launchSelfishMining(i, j):
+    inputVariables = {'numberCycles':100, 'relativeHashrate':j, 'gamma':i,'bitcoinValue':30000,'bitcoinReward':6.25}
+    new = SelfishMining(**inputVariables)
+    new.Simulation1()
+    return new.profitSelfishMining/(new.officialChain*new.bitcoinReward*new.bitcoinValue)
+
+#### Display graphic #### 
+def createGraphic(kmax): 
+    for i in range(0, 3, 1):
+        x = []
+        y = []
+        for j in range(0, 500, 1):
+            moyenneX = 0
+            moyenneY = 0
+            if(i == 0):
+                for k in range(1, kmax, 1):
+                    moyenneX += j*0.001
+                    moyenneY +=launchSelfishMining(0, j*0.001)
+                x.append(moyenneX / kmax)
+                y.append(moyenneY / kmax)
+            elif(i == 1):
+                for k in range(1, kmax, 1):
+                    moyenneX += j*0.001
+                    moyenneY +=launchSelfishMining(0.5, j*0.001)
+                x.append(moyenneX / kmax)
+                y.append(moyenneY / kmax)
+            elif(i == 2):
+                for k in range(1, kmax, 1):
+                    moyenneX += j*0.001
+                    moyenneY +=launchSelfishMining(1, j*0.001)
+                x.append(moyenneX / kmax)
+                y.append(moyenneY / kmax)
+
+        # DISPLAY THE GRAPHIC
+        curbX = np.array(x)
+        curbY = np.array(y)
+        plt.plot(curbX, curbY)
+
+    plt.title('Revenue of SelfishMining strategy')
+    plt.xlabel('Relative pool revenue')         # Nom de la grandeur en abscisse
+    plt.ylabel('Pool size')         # Nom de la grandeur en ordonnée
+
+    plt.show()
+
+def pause():
+    input("Press the <ENTER> key to continue...")
+
+
+#### MENIU ####
+ans=True
+data = []
+while ans:
+    print ("""
+    1.Create a graphic of the reward of selfish miners compare to honest miners
+    2.Launch a selfish mining test for default data 
+    3.Launch a selfish mining test for input data 
+    4.Exit
+    """)
+    ans=input("What would you like to do? ") 
+    if ans=="1": 
+      createGraphic(10)
+    elif ans=="2":
+      data = testSelfishMiningAutomaticData()
+      displayData(data)
+    elif ans=="3":
+      data = testSelfishMiningInputData()
+      displayData(data)
+      pause()
+    elif ans=="4":
+      print("\n Goodbye")
+      import sys
+      sys.exit(0)
+    elif ans !="":
+      print("\n Not Valid Choice Try again") 
+
+
+
+
+
 
